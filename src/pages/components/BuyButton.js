3459911ef2e1cf87c394fbad2f6dee500ea6e87e
DEHarshit <harshit.s2004@gmail.com>
2024-06-13 09:58:59 +0530

    added loading to buy button

--- src/pages/components/BuyButton.js
+++ src/pages/components/BuyButton.js
@@ -3,18 +3,21 @@ import { useAccount, useConnect, useDisconnect } from 'wagmi';
 import { ethers } from 'ethers';
 import { sepolia } from 'wagmi/chains';
 import urform from './urform.json';
+import { useState } from 'react'
 
 
-function BuyButton ({id,userid,sellerid,price}) {
+function BuyButton({ id, userid, sellerid, price }) {
+  const [loading, setLoading] = useState(false);
   const { address, isConnected } = useAccount();
   const { connect, connectors } = useConnect();
   const { disconnect } = useDisconnect();
 
-  const seller = urform.find((e)=>e.userid === sellerid)
+  const seller = urform.find((e) => e.userid === sellerid)
 
   const handleBuy = async () => {
+    setLoading(true)
     if (!isConnected) {
-      connect(connectors[0]); 
+      connect(connectors[0]);
       return;
     }
     const garbageAddress = seller.useraddr;
@@ -24,41 +27,42 @@ function BuyButton ({id,userid,sellerid,price}) {
     try {
       const tx = await signer.sendTransaction({
         to: garbageAddress,
-        value: ethers.utils.parseEther(`${price}`) 
+        value: ethers.utils.parseEther(`${price}`)
       });
-      await tx.wait(); 
+      await tx.wait();
       alert('Transaction successful! 0.01 ETH has been sent.');
       try {
         const response = await fetch('http://localhost:3000/api/update-owner', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ id, newName: userid }), // Change 'nabeel' to the new owner's name
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ id, newName: userid }), // Change 'nabeel' to the new owner's name
         });
 
         console.log(JSON.stringify({ id, newName: userid }));
 
         if (response.ok) {
-            const data = await response.json();
-            alert(`Congratulations! You have successfully purchased.`);
+          const data = await response.json();
+          alert(`Congratulations! You have successfully purchased.`);
         } else {
-            const errorData = await response.json();
-            alert(`Failed to update owner: ${errorData.error}`);
+          const errorData = await response.json();
+          alert(`Failed to update owner: ${errorData.error}`);
         }
-    } catch (error) {
+      } catch (error) {
+        setLoading(false)
         console.error('Error buying NFT:', error);
         alert('Failed to update owner: An error occurred.');
-    }
+      }
     } catch (error) {
       console.error(error);
       alert('Transaction failed!');
     }
-    
+
   };
 
-  if (!seller){
-    return(
+  if (!seller) {
+    return (
       <div>
         Loading...
       </div>
@@ -67,7 +71,10 @@ function BuyButton ({id,userid,sellerid,price}) {
   return (
     <div>
       {isConnected ? (
-        <button onClick={handleBuy} className="font-primary h-12 w-36 bg-blue-600 hover:bg-blue-500 transition-all text-white font-bold py-2 px-4 rounded-md">Buy</button>
+        loading
+          ? <button className="font-primary h-12 w-36 bg-blue-600 cursor-progress transition-all text-white font-bold py-2 px-4 rounded-md">Loading...</button>
+          : <button onClick={handleBuy} className="font-primary h-12 w-36 bg-blue-600 hover:bg-blue-500 transition-all text-white font-bold py-2 px-4 rounded-md">Buy</button>
+
       ) : (null)}
     </div>
   );
